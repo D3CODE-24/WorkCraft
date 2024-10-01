72479d06002cf434eaa7a7fb9386b6be698d1073
Voidknight-2004 <rohanajacobgdsc@gmail.com>
2024-10-01 23:10:29 +0530

    e-commerce basic framework

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="text-center">
+    <div className="text-center mt-8">
     <h1 className="text-4xl font-bold mb-4">Welcome to WorkCraft</h1>
     <p className="text-xl">Empowering unorganized workers and small-scale producers</p>
     </div>
