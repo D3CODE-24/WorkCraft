1a2e228d457bebe4530a920d00efb6a971d66676
Roshin R <139961141+RoshinR2005@users.noreply.github.com>
2024-10-06 03:56:17 +0530

    Updated Hero Section

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -18,19 +18,18 @@ const HeroSection = () => {
     return (
         <div className='text-center'>
             <div className='flex flex-col gap-5 my-10'>
-                <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
-                <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur temporibus nihil tempora dolor!</p>
+                <h1 className='text-5xl font-bold'>Discover, Apply & <br /> Build a <span className='text-[#EE4865]'>Brighter Future</span></h1>
+                <p className="text-lg">Empowering Unorganized Sector Workers for a Better Tomorrow</p>
                 <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
                         type="text"
-                        placeholder='Find your dream jobs'
+                        placeholder='Find jobs suited to your skills'
                         onChange={(e) => setQuery(e.target.value)}
                         className='outline-none border-none w-full'
 
                     />
-                    <Button onClick={searchJobHandler} className="rounded-r-full bg-[#6A38C2]">
-                        <Search className='h-5 w-5' />
+                    <Button onClick={searchJobHandler} className="rounded-r-full  bg-[#EE4865]">
+                        <Search className='h-5 w-' />
                     </Button>
                 </div>
             </div>
