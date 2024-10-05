623ad874ad7df58b771e6ff4f77b57f3a88ca113
Roshin R <139961141+RoshinR2005@users.noreply.github.com>
2024-10-06 04:23:25 +0530

    Corrected Navbar and Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import banner from "../assets/banner.png";
+import Footer from "@/components/shared/Footer";
 
 const Home = () => {
   return (
-    <div className="text-center mt-0 ">
+    <div className="text-center mt-0 w-full">
       <img src={banner} alt="banner" className="-mt-7" />
+      <Footer/>
     </div>
   );
 };
