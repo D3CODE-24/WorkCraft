1fdd07dc12ec3dd30d0ea17a227643b452c58731
sharon504 <sharonpshajan@gmail.com>
2024-10-05 13:04:16 +0530

    Adding stripe functionality

--- server/src/ecommerece/routes/order-routes.js
+++ server/src/ecommerece/routes/order-routes.js
@@ -6,5 +6,6 @@ const OrderRouter = Router();
 OrderRouter.post("/create-order", OrderController.cerateOrder);
 OrderRouter.get("/", OrderController.getOrders);
 OrderRouter.post("/:id/mark-shipped", OrderController.shipping);
+OrderRouter.post("create-payment", OrderController.createPayment);
 
 export default OrderRouter;
