3fa6fa306840a068282be68b1c381492bbde25a7
Roshin R <139961141+RoshinR2005@users.noreply.github.com>
2024-10-06 22:18:44 +0530

    updated constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,4 +2,4 @@ export const USER_API_END_POINT="https://workcraft-z2am.onrender.com/api/jobPort
 export const JOB_API_END_POINT="https://workcraft-z2am.onrender.com/api/jobPortal/job";
 export const APPLICATION_API_END_POINT="https://workcraft-z2am.onrender.com/api/jobPortal/application";
 export const COMPANY_API_END_POINT="https://workcraft-z2am.onrender.com/api/jobPortal/company";
-export const MARKET_ANALYSIS_API_END_POINT="http://localhost:5000/api";
+export const MARKET_ANALYSIS_API_END_POINT="https://workcraft-16hi.onrender.com/api";
