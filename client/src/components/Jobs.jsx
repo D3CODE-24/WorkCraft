42f19c2533ab6e4b573a52fb22b227b35f5112e5
Roshin R <139961141+RoshinR2005@users.noreply.github.com>
2024-10-06 14:57:12 +0530

    Updated Jobs

--- client/src/components/Jobs.jsx
+++ client/src/components/Jobs.jsx
@@ -25,7 +25,7 @@ const Jobs = () => {
 
     return (
         <div>
-            <div className='max-w-7xl mx-auto mt-5'>
+            <div className=' mx-auto mt-5'>
                 <div className='flex gap-5'>
                     <div className='w-20%'>
                         <FilterCard />
