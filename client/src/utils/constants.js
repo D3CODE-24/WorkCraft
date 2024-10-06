b35f0b4a710b4e8c0886d4854512efdfaf996d47
sharon504 <sharonpshajan@gmail.com>
2024-10-06 22:53:50 +0530

    Fixed typo in client utils using https instead of http

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,8 @@
-export const USER_API_END_POINT="https://localhost:3000/api/jobPortal/user";
-export const JOB_API_END_POINT="https://localhost:3000/api/jobPortal/job";
-export const APPLICATION_API_END_POINT="https://localhost:3000/api/jobPortal/application";
-export const COMPANY_API_END_POINT="https://localhost:3000/api/jobPortal/company";
-export const MARKET_ANALYSIS_API_END_POINT="https://localhost:5000/api";
+export const USER_API_END_POINT = "http://localhost:3000/api/jobPortal/user";
+export const JOB_API_END_POINT = "http://localhost:3000/api/jobPortal/job";
+export const APPLICATION_API_END_POINT =
+  "http://localhost:3000/api/jobPortal/application";
+export const COMPANY_API_END_POINT =
+  "http://localhost:3000/api/jobPortal/company";
+export const MARKET_ANALYSIS_API_END_POINT = "http://localhost:5000/api";
+
